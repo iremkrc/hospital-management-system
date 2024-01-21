ad1b2169b9caefd53382a391ef543fb6b5665a88
iremkrc <ikaraca19@ku.edu.tr>
2024-01-21 15:03:25 +0300

    Delete doctor patient and button functionalities

--- Admin.py
+++ Admin.py
@@ -10,7 +10,7 @@ class AdminFrame(tk.Frame):
         self.db_connection = mysql.connector.connect(
         host="localhost",
         user="root",
-        passwd="24632463",
+        passwd="pwd",
         auth_plugin='mysql_native_password'
         )
         self.db_cursor = self.db_connection.cursor(buffered=True)
